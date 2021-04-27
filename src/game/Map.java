95da42c0b65f5de66b3c4f499bcb1f6a334d1edf
yhjava00 <yhjava00@gmail.com>
2021-04-27 19:50:03 +0900

    init commit

--- src/game/Map.java
+++ src/game/Map.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -21,6 +22,10 @@ public class Map extends JPanel implements ActionListener{
 	private final int ITEM_SIZE = 10;
 	private final int SPEED = 30;
 
+	private Set<int[]> bulletOnField;
+	private Set<int[]> removeBullet;
+	
+	
 	private int[] p1, p2;
 
 	private Timer timer;
@@ -43,24 +48,61 @@ public class Map extends JPanel implements ActionListener{
 	private void startGame() {
 		addKeyListener(gunFightAdapter);
 		
+		bulletOnField = new HashSet<>();
+		removeBullet = new HashSet<>();
+		
 		p1 = new int[] {0, MAP_HEIGHT/2, 1};
 		p2 = new int[] {MAP_WIDTH-1, MAP_HEIGHT/2, 3};
 		
 		timer.start();
 	}
 
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		g.setColor(Color.red);
-		g.fillRect(p1[0]*ITEM_SIZE, p1[1]*ITEM_SIZE, ITEM_SIZE, ITEM_SIZE);
-
-		g.setColor(Color.blue);
-		g.fillRect(p2[0]*ITEM_SIZE, p2[1]*ITEM_SIZE, ITEM_SIZE, ITEM_SIZE);
-		
+	private void createBullet(int[] p) {
+		switch (p[2]) {
+		case 1:
+			bulletOnField.add(new int[] {p[0]+1, p[1], p[2]});
+			break;
+		case 2:
+			bulletOnField.add(new int[] {p[0], p[1]+1, p[2]});
+			break;
+		case 3:
+			bulletOnField.add(new int[] {p[0]-1, p[1], p[2]});
+			break;
+		case 4:
+			bulletOnField.add(new int[] {p[0], p[1]-1, p[2]});
+			break;
+		}
 	}
 
+	private void bulletMove() {
+		for(int[] i : bulletOnField) {
+			switch (i[2]) {
+			case 1:
+				i[0]++;
+				break;
+			case 2:
+				i[1]++;
+				break;
+			case 3:
+				i[0]--;
+				break;
+			case 4:
+				i[1]--;
+				break;
+			}
+			if(checkOut(i))
+				removeBullet.add(i);
+		}
+		removeBullet();
+	}
+
+	private void removeBullet() {
+		for(int[] i : removeBullet) {
+			bulletOnField.remove(i);
+		}
+		removeBullet.clear();
+	}
+	
 	private void movePlayer(int[] p, int direction) {
 		
 		p[2] = direction;
@@ -79,13 +121,54 @@ public class Map extends JPanel implements ActionListener{
 			p[1]--;
 			break;
 		}
+		
+		if(checkOut(p)||(p1[0]==p2[0]&&p1[1]==p2[1])) {
+			switch (p[2]) {
+			case 1:
+				p[0]--;
+				break;
+			case 2:
+				p[1]--;
+				break;
+			case 3:
+				p[0]++;
+				break;
+			case 4:
+				p[1]++;
+				break;
+			}
+		}
+	}
+
+	private boolean checkOut(int[] item) {
+		if((item[0]<0||item[0]>=MAP_WIDTH)||(item[1]<0||item[1]>=MAP_HEIGHT))
+			return true;
+		return false;
 	}
 	
 	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(Color.red);
+		g.fillRect(p1[0]*ITEM_SIZE, p1[1]*ITEM_SIZE, ITEM_SIZE, ITEM_SIZE);
+
+		g.setColor(Color.blue);
+		g.fillRect(p2[0]*ITEM_SIZE, p2[1]*ITEM_SIZE, ITEM_SIZE, ITEM_SIZE);
+
+		g.setColor(Color.yellow);
+		for(int[] i : bulletOnField) {
+			g.fillRect(i[0]*ITEM_SIZE, i[1]*ITEM_SIZE, ITEM_SIZE, ITEM_SIZE);
+		}
+		
+	}
+
+	@Override
 	public void actionPerformed(ActionEvent e) {
+		bulletMove();
 		repaint();
 	}
-	
+
 	class GunFightAdapter extends KeyAdapter {
 		
 		@Override
@@ -106,6 +189,7 @@ public class Map extends JPanel implements ActionListener{
             	movePlayer(p1, 4);
             	break;
             case KeyEvent.VK_SPACE:
+            	createBullet(p1);
             	break;
             case KeyEvent.VK_RIGHT:
             	movePlayer(p2, 1);
@@ -120,10 +204,9 @@ public class Map extends JPanel implements ActionListener{
             	movePlayer(p2, 4);
             	break;
             case KeyEvent.VK_NUMPAD0:
+            	createBullet(p2);
             	break;
             }
-		}
-		    	
+		} 	
     }
-
 }
